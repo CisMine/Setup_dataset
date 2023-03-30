d07d8808342e6172032cede2096a715984d0a6b1
CisMine <ngogiahuy20012004@gmail.com>
2023-03-30 17:24:11 +0700

    Commit2

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
-import numpy as np
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+from torchvision.transforms import Compose, ToTensor, Resize
 
-print('hello')
 
-print('lol')
-print('by')
+
+class AnimalDataset(Dataset):
+    def __init__(self, root, train=True, transform=None):
+        self.image_paths = []
+        self.labels = []
+        self.categories = ["butterfly", "cat", "chicken", "cow", "dog", "elephant", "horse", "sheep", "spider","squirrel"]
+        self.transform = transform
+
+        data_path = os.path.join(root, "animals")
+
+        if train:
+            data_path = os.path.join(data_path, "train")
+        else:
+            data_path = os.path.join(data_path, "test")
+
+        for i, category in enumerate(self.categories):
+            data_files = os.path.join(data_path, category)
+            for item in os.listdir(data_files):
+                path = os.path.join(data_files, item)
+                self.image_paths.append(path)
+                self.labels.append(i)
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        image_path = self.image_paths[index]
+        image = Image.open(image_path).convert('RGB')
+        label = self.labels[index]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+
+if __name__ == "__main__":
+    transform = Compose([
+        Resize((224, 224)),
+        ToTensor(),
+    ])
+    dataset = AnimalDataset(root="../data", train=True, transform=transform)
+    index = 4440
+    image, label = dataset.__getitem__(index)
+    print(image.shape)
+    print(label)
